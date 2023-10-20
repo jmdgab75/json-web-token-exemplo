00a31e942fa63c829b5f26cd31b2dcf7a02703d9
jmdgab75 <gabrieljmd75@gmail.com>
2023-10-20 17:09:43 -0400

    commit 2010

--- servidor/index.js
+++ servidor/index.js
@@ -48,7 +48,7 @@ app.get('/usuarios/cadastrar', async function(req, res){
 app.post('/usuarios/cadastrar', async function(req, res){
   try { 
  const criptografia = {
-   nome: req.body.name,
+   nome: req.body.nome,
    senha: crypto.encrypt(req.body.senha)
 }
     if(req.body.senha == req.body.csenha){
